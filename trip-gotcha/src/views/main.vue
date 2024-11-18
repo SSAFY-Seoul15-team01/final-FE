bcd201c632451074a6bb5902e7fd4bc3cb0c041e
송정호 <dynamite885@naver.com>
2024-11-18 14:54:54 +0900

    init: 프로젝트 생성 및 shadcn 설정

---
+++ trip-gotcha/src/views/main.vue
@@ -0,0 +1,11 @@
+<script setup>
+import GNB from "@/components/Gnb.vue";
+import MobileLayout from "@/components/Layout.vue";
+</script>
+
+<template>
+    <MobileLayout>
+        <div class="grow bg-black"></div>
+        <GNB />
+    </MobileLayout>
+</template>
