a8693d7ea4387b1dc4edfbba991676e2a2be2a43
최수현 <sw92522@naver.com>
2024-11-25 16:05:21 +0900

    feat: 지도 생성

--- trip-gotcha/src/views/main.vue
+++ trip-gotcha/src/views/main.vue
@@ -2,12 +2,24 @@
 import BottomSheet from "@/components/BottomSheet.vue";
 import GNB from "@/components/Gnb.vue";
 import MobileLayout from "@/components/Layout.vue";
+import ArticleList from "@/components/ArticleList.vue";
+import { KakaoMap, KakaoMapMarker } from 'vue3-kakao-maps';
+const coordinate = {
+  lat: 37.566826,
+  lng: 126.9786567
+};
 </script>
 
 <template>
     <MobileLayout>
-        <div class="grow bg-black"></div>
+        <div class="grow bg-black">
+            <KakaoMap :width="450" :height="1000" :lat="coordinate.lat" :lng="coordinate.lng" :draggable="true">
+                <KakaoMapMarker :lat="coordinate.lat" :lng="coordinate.lng"></KakaoMapMarker>
+            </KakaoMap>
+        </div>
         <GNB />
-        <BottomSheet/>
+        <BottomSheet>
+            <ArticleList/>
+        </BottomSheet>
     </MobileLayout>
 </template>
